9a3bc21580ebe02c4566713ea0d9ad1fa684c098
adeeburrahman11 <adeeburrahman9021@gmail.com>
2025-01-10 16:37:31 +0530

    color pallete set

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+import tailwindScrollbar from "tailwind-scrollbar";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        "baby-blue": "#ECF1F2",
+        "cool-grey": "#53565A",
+        "a-yellow": "#F4D800",
+        "a-black": "#050707",
+      },
+    },
   },
-  plugins: [],
+  plugins: [tailwindScrollbar],
 };
