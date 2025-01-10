71ca98044a6fff012fa61b0749177330f4dacea1
adeeburrahman11 <adeeburrahman9021@gmail.com>
2025-01-10 23:22:40 +0530

    menu btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import Header from "./components/Header";
+
 const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <Header />;
 };
 
 export default App;
