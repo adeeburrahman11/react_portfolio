940900bd78489dee045aa216273966793c8c4496
adeeburrahman11 <adeeburrahman9021@gmail.com>
2025-01-11 23:45:18 +0530

    .

---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section id="home" className="">
+      <div className="">
+        <div>
+          <div className="">
+            <figure className="">
+              <img
+                src="/images/avatar-1.jpg"
+                width={40}
+                height={40}
+                alt="Adeebur Rahman Portrait"
+                className="img-cover"
+              />
+            </figure>
+            <div className="">
+              <span className="">
+                <span className=""></span>
+              </span>
+              Available for work
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
