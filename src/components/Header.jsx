5668eb77b6d9248b023a80d30fddd13f3616c7f4
adeeburrahman11 <adeeburrahman9021@gmail.com>
2025-01-11 17:11:52 +0530

    toggle menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { useState } from "react";
 
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-baby-blue to-baby-blue/0">
       <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md-grid md:grid-cols-[1fr,3fr,1fr]">
@@ -16,12 +19,20 @@ const Header = () => {
           </a>
         </h1>
         <div className="relative md:justify-self-center">
-          <button className="menu-btn md:hidden" onClick={{}}>
-            <span className="material-symbols-rounded">menu</span>
+          <button
+            className="menu-btn md:hidden"
+            onClick={() => setNavOpen((prev) => !prev)}
+          >
+            <span className="material-symbols-rounded">
+              {navOpen ? "close" : "menu"}
+            </span>
           </button>
-          <Navbar />
+          <Navbar navOpen={navOpen} />
         </div>
-        <a href="#contact" className="">
+        <a
+          href="#contact"
+          className="btn btn-secondary max-md:hidden md:justify-self-end"
+        >
           Contact Me
         </a>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
+import PropTypes from "prop-types";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = ({ navOpen }) => {
+  const lastActiveLink = useRef();
+  const activeBox = useRef();
+
+  const navItems = [
+    {
+      label: "Home",
+      link: "#home",
+      className: "nav-link active",
+      ref: lastActiveLink,
+    },
+    {
+      label: "About",
+      link: "#about",
+      className: "nav-link",
+    },
+    {
+      label: "Projects",
+      link: "#projects",
+      className: "nav-link",
+    },
+    {
+      label: "Experience",
+      link: "#experience",
+      className: "nav-link",
+    },
+    {
+      label: "Contact",
+      link: "#contact",
+      className: "nav-link md:hidden",
+    },
+  ];
+
+  return (
+    <nav className={"navbar " + (navOpen ? "active" : "")}>
+      {navItems.map(({ label, link, className, ref }, key) => (
+        <a href={link} className={className} key={key} ref={ref} onClick={{}}>
+          {label}
+        </a>
+      ))}
+      <div className="active-box" ref={activeBox}></div>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  navOpen: PropTypes.bool.isRequired,
 };
 
 export default Navbar;
