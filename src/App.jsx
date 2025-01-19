3de70cd639955934510a84910d3df8566441e872
adeeburrahman11 <adeeburrahman9021@gmail.com>
2025-01-19 21:19:22 +0530

    projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Hero />
         <About />
         <Skills />
+        <Projects />
       </main>
     </>
   );
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ProjectCard = ({ imgSrc, title, tags, projectLink, classes }) => {
+  return (
+    <div
+      className={
+        "relative p-4 rounded-2xl bg-a-black/5 hover:bg-a-black/10 active:bg-a-black/15 ring-1 ring-inset ring-a-black/20 transition-colors " +
+        classes
+      }
+    >
+      <figure className="img-box aspect-square rounded-lg mb-4">
+        <img src={imgSrc} loading="lazy" alt={title} className="img-cover" />
+      </figure>
+      <div className="flex items-center justify-between gap-4">
+        <div>
+          <h3 className="title-1 mb-3">{title}</h3>
+          <div className="flex flex-wrap items-center gap-2">
+            {tags.map((label, index) => (
+              <span
+                key={index}
+                className="h-8 text-sm text-cool-grey bg-a-black/15 grid items-center px-3 rounded-lg"
+              >
+                {label}
+              </span>
+            ))}
+          </div>
+        </div>
+        <div className="w-11 h-11 rounded-lg grid place-items-center bg-a-yellow text-cool-grey shrink-0">
+          <span className="material-symbols-rounded" aria-hidden="true">
+            arrow_outward
+          </span>
+        </div>
+      </div>
+      <a href={projectLink} className="absolute inset-0" target="_blank"></a>
+    </div>
+  );
+};
+
+ProjectCard.propTypes = {
+  imgSrc: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  projectLink: PropTypes.string,
+  classes: PropTypes.string,
+};
+
+export default ProjectCard;
+// import React from "react";
+// import PropTypes from "prop-types";
+
+// const ProjectCard = ({ imgSrc, title, tags = [], projectLink, classes }) => {
+//   return (
+//     <div className={"" + classes}>
+//       <figure className="">
+//         <img src={imgSrc} loading="lazy" alt={title} className="img-cover" />
+//       </figure>
+//       <div className="">
+//         <div>
+//           <h3 className="title-1">{title}</h3>
+//           <div className="">
+//             {tags.map((label, index) => (
+//               <span key={index} className="">
+//                 {label}
+//               </span>
+//             ))}
+//           </div>
+//         </div>
+//         <div>
+//           <span className="material-symbol-rounded" aria-hidden="true">
+//             arrow_outward
+//           </span>
+//         </div>
+//       </div>
+//       <a href={projectLink} className="" target="_blank"></a>
+//     </div>
+//   );
+// };
+
+// ProjectCard.propTypes = {
+//   imgSrc: PropTypes.string.isRequired,
+//   title: PropTypes.string.isRequired,
+//   tags: PropTypes.arrayOf(PropTypes.string),
+//   projectLink: PropTypes.string,
+//   classes: PropTypes.string,
+// };
+
+// export default ProjectCard;
